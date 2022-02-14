3a63ee94cae491eb2aa303650c23bd15a07c7348
SGrossett <selene_816@hotmail.com>
2022-02-13 21:48:41 -0500

    Add icons to home sidebar and render it in the index

--- pomify/components/HomeSidebar.js
+++ pomify/components/HomeSidebar.js
@@ -1,7 +1,40 @@
+import { FaClipboardList, FaRegChartBar, FiUser } from "react-icons/fa";
+import { IoHomeOutline, IoSettingsOutline, IoGameControllerOutline } from 'react-icons/io5';
+import { FiLogOut } from "react-icons/fi";
+
 function HomeSidebar() {
   return (
     <div>
-      <h1>HomeSidebar</h1>
+      <div>
+        <button>
+          <IoHomeOutline />
+          <p>Home</p>
+        </button>
+      </div>
+      <div>
+        <button>
+          <FaRegChartBar />
+          <p>Progress</p>
+        </button>
+      </div>
+      <div>
+        <button>
+          <IoGameControllerOutline />
+          <p>Break</p>
+        </button>
+      </div>
+      <div>
+        <button>
+          <IoSettingsOutline />
+          <p>Settings</p>
+        </button>
+      </div>
+      <div>
+        <button>
+          <FiLogOut />
+          <p>Logout</p>
+        </button>
+      </div>
     </div>
   )
 }
