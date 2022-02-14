138b7483a0ef70987cd45dec28dc7d03c7195aa2
SGrossett <selene_816@hotmail.com>
2022-02-14 12:42:25 -0500

    Create a spotify helpers functions file and add authorization scopes that will determine what information spotify users will share with the pomify app.

---
+++ pomify/helpers/spotify.js
@@ -0,0 +1,17 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+const scopes = [
+  "user-read-email",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "user-read-email",
+  "streaming",
+  "user-read-private",
+  "user-library-read",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "user-read-currently-playing",
+  "user-read-currently-played",
+  "user-follow-to-read"
+].join(", ");
+
