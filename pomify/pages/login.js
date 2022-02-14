2fdeeca784a9df3f2cc67bf55afc30491533990e
SGrossett <selene_816@hotmail.com>
2022-02-14 14:10:35 -0500

    Create a server side render by getting the latest providers and delivering it to the client.

--- pomify/pages/login.js
+++ pomify/pages/login.js
@@ -1,7 +1,19 @@
+import {getProviders, signIn } from 'next-auth/react';
+
 function Login() {
   return (
     <div>This is the login page</div>
   )
 }
 
-export default Login
+export default Login;
+
+export async function GetServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers
+    }
+  }
+}
