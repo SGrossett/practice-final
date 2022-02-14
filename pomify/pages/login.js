ccb5b396a9cfd410abfbb829beca18755d95a000
SGrossett <selene_816@hotmail.com>
2022-02-14 17:59:02 -0500

    Display providers with spotify logo.

--- pomify/pages/login.js
+++ pomify/pages/login.js
@@ -1,14 +1,22 @@
 import {getProviders, signIn } from 'next-auth/react';
 
-function Login() {
+function Login( { providers }) {
   return (
-    <div>This is the login page</div>
+    <div>
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button>Login with {provider.name}</button>
+        </div>
+      ))}
+    </div>
   )
 }
 
 export default Login;
 
-export async function GetServerSideProps() {
+export async function getServerSideProps() {
   const providers = await getProviders();
 
   return {
