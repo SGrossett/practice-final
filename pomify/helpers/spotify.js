441bf88ae6e29b51cbd8311fcab4bd39f1632e70
SGrossett <selene_816@hotmail.com>
2022-02-14 12:50:40 -0500

    Create and export  a login url to query the scope params. Configure and export Sptify API.

--- pomify/helpers/spotify.js
+++ pomify/helpers/spotify.js
@@ -15,3 +15,18 @@ const scopes = [
   "user-follow-to-read"
 ].join(", ");
 
+const params = {
+  scope: scopes,
+};
+
+const queryParamsString = new URLSearchParams(params);
+
+const LOGIN_URL = `https://account.spotify.com/authorize?${queryParamsString.toString()}`;
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET
+});
+
+export default spotifyApi;
+export { LOGIN_URL };
