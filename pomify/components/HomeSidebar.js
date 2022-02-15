f234ac0dc554b470435e1e1c1958129a49387c9d
SGrossett <selene_816@hotmail.com>
2022-02-14 22:06:47 -0500

    Fix the async session callback so that correct user tokens are being passed down.

--- pomify/components/HomeSidebar.js
+++ pomify/components/HomeSidebar.js
@@ -1,43 +1,49 @@
-import { FaClipboardList, FaRegChartBar, FiUser } from "react-icons/fa";
+import { FaClipboardList, FaRegChartBar, FiUser } from 'react-icons/fa';
 import { IoHomeOutline, IoSettingsOutline, IoGameControllerOutline } from 'react-icons/io5';
-import { FiLogOut } from "react-icons/fi";
+import { FiLogOut } from 'react-icons/fi';
+import { signOut, useSession } from 'next-auth/react';
 
 function HomeSidebar() {
+  const { data: session, status } = useSession();
+  console.log(session);
+
   return (
-    <div className="text-gray-500 p-5 border-gray-900">
-      <div className="space-y-8">
-        <div className="space-y-4">
-          <button className="flex items-center space-x-2 hover:text-white">
-            <IoHomeOutline size="1.5em"/>
+    <div className='text-gray-500 p-5 border-gray-900'>
+      <div className='space-y-8'>
+        <div className='space-y-4'>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <IoHomeOutline size='1.5em'/>
             <p>Home</p>
           </button>  
-          <button className="flex items-center space-x-2 hover:text-white">
-            <FaRegChartBar size="1.5em"/>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <FaRegChartBar size='1.5em'/>
             <p>Progress</p>
           </button>
-          <button className="flex items-center space-x-2 hover:text-white">
-            <IoGameControllerOutline size="1.5em"/>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <IoGameControllerOutline size='1.5em'/>
             <p>Break</p>
           </button>
         </div>
 
-        <div className="space-y-4">
-          <button className="flex items-center space-x-2 hover:text-white">
-            <IoSettingsOutline size="1.5em"/>
+        <div className='space-y-4'>
+          <button className='flex items-center space-x-2 hover:text-white'>
+            <IoSettingsOutline size='1.5em'/>
             <p>Settings</p>
           </button>
-          <button className="flex items-center space-x-2 hover:text-white">
-            <FiLogOut size="1.5em"/>
+          <button className='flex items-center space-x-2 hover:text-white'
+            onClick={() => signOut()}
+          >
+            <FiLogOut size='1.5em'/>
             <p>Logout</p>
           </button>
 
-          <hr className="border-t-[0.1px] border-gray-900" />
+          <hr className='border-t-[0.1px] border-gray-900' />
 
-          <p className="cursor-pointer hover:text-white">Playlist name...</p>
-          <p className="cursor-pointer hover:text-white">Playlist name...</p>
-          <p className="cursor-pointer hover:text-white">Playlist name...</p>
-          <p className="cursor-pointer hover:text-white">Playlist name...</p>
-          <p className="cursor-pointer hover:text-white">Playlist name...</p>
+          <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+          <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+          <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+          <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+          <p className='cursor-pointer hover:text-white'>Playlist name...</p>
         </div>
       </div>
     </div>
