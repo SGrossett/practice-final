7e6a89a7b49a0108aee8f0551d29b52edf158c29
SGrossett <selene_816@hotmail.com>
2022-02-15 10:53:57 -0500

    Create a hook to set the access token for the api that this going to be used through out the app to keep track of the user session.

---
+++ pomify/hooks/useSpotify.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+import spotifyApi from '../helpers/spotify';
+
+function useSpotify() {
+
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      if (session.error === "RefreshAccessToken") {
+        signIn();
+      }
+
+      spotifyApi.setAccessToken(session.user.accessToken);
+    }
+  }, []);
+
+  return spotifyApi;
+}
+
+export default useSpotify
