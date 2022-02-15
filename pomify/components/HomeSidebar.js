0d6cb8edf315bb5cca2935c4544ddb2cb37c955d
SGrossett <selene_816@hotmail.com>
2022-02-15 12:17:36 -0500

    Map through playlists and have them displa on home sidebar.

--- pomify/components/HomeSidebar.js
+++ pomify/components/HomeSidebar.js
@@ -58,11 +58,10 @@ function HomeSidebar() {
 
           <hr className='border-t-[0.1px] border-gray-900' />
 
-          <p className='cursor-pointer hover:text-white'>Playlist name...</p>
-          <p className='cursor-pointer hover:text-white'>Playlist name...</p>
-          <p className='cursor-pointer hover:text-white'>Playlist name...</p>
-          <p className='cursor-pointer hover:text-white'>Playlist name...</p>
-          <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+          {playlists.map((playlist) => (
+            <p key={playlist.id} className='cursor-pointer hover:text-white'>{playlist.name}</p>
+          ))}
+          
         </div>
       </div>
     </div>
