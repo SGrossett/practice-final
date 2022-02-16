342e2d51ac5e7e370fec71500ed0b214ab50e7e3
SGrossett <selene_816@hotmail.com>
2022-02-16 01:31:53 -0500

    Add functionality to logout button.

--- pomify/components/UserIcon.js
+++ pomify/components/UserIcon.js
@@ -1,6 +1,6 @@
 import { Menu, Transition } from '@headlessui/react'
 import { IoChevronDownSharp } from 'react-icons/io5';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { Fragment } from 'react'
 
 
@@ -8,8 +8,8 @@ export default function Example() {
   const { data: session } = useSession();
 
   return (
-    <div className="w-56 text-right fixed top-5 right-8">
-      <Menu as="div" className="relative inline-block text-left">
+    <div className='w-56 text-right fixed top-5 right-8'>
+      <Menu as='div' className='relative inline-block text-left'>
         <div>
           <Menu.Button className='flex items-center bg-black text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full pr-1'>
             <div className='flex items-center bg-black text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-1'>
@@ -19,26 +19,26 @@ export default function Example() {
                 alt='' 
               />
               <h2>{session?.user.name}</h2>
-              <IoChevronDownSharp className="ml-2 -mr-1 text-white" />
+              <IoChevronDownSharp className='ml-2 -mr-1 text-white' />
             </div>
           </Menu.Button>
         </div>
         <Transition
           as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
+          enter='transition ease-out duration-100'
+          enterFrom='transform opacity-0 scale-95'
+          enterTo='transform opacity-100 scale-100'
+          leave='transition ease-in duration-75'
+          leaveFrom='transform opacity-100 scale-100'
+          leaveTo='transform opacity-0 scale-95'
         >
-          <Menu.Items className="absolute right-0 w-36 mt-2 origin-top-right opacity-90 hover:opacity-80 bg-black text-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className='absolute right-0 w-36 mt-2 origin-top-right opacity-90 hover:opacity-80 bg-black text-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
               
-            <div className="px-1 py-1">
+            <div className='px-1 py-1'>
               <Menu.Item>
                 {({ active }) => (
                   <a
-                    href="/tester"
+                    href='/tester'
                     className={`${
                       active ? 'bg-gray-900' : ''
                     } group flex rounded-md items-center w-full px-2 py-2 text-sm text-white`}
@@ -50,7 +50,7 @@ export default function Example() {
               <Menu.Item>
                 {({ active }) => (
                   <a
-                    href="/tester"
+                    href='/tester'
                     className={`${
                       active ? 'bg-gray-900' : ''
                     } group flex rounded-md items-center w-full px-2 py-2 text-sm text-white`}
@@ -60,17 +60,17 @@ export default function Example() {
                 )}
               </Menu.Item>
             </div>
-            <div className="px-1 py-1">
+            <div className='px-1 py-1'>
               <Menu.Item>
               {({ active }) => (
-                  <a
-                    href="/tester"
+                  <button
+                    onClick={signOut}
                     className={`${
-                      active ? 'bg-gray-900' : ''
-                    } group flex rounded-md items-center w-full px-2 py-2 text-sm text-white`}
-                  >
+                        active ? 'bg-gray-900' : ''
+                      } group flex rounded-md items-center w-full px-2 py-2 text-sm text-white`}
+                    >
                     Log out
-                  </a>
+                  </button>
                 )}
               </Menu.Item>
             </div>
