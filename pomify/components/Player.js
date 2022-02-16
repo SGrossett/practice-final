62766435cbe783c45e6278443c0f67d2637f7275
SGrossett <selene_816@hotmail.com>
2022-02-16 03:17:21 -0500

    Create a custom hook to grab song info. Use the hook to display the song image for the player bar.

--- pomify/components/Player.js
+++ pomify/components/Player.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
+import { useSession } from 'next-auth/react';
+import { useRecoilState } from 'recoil';
+import useSpotify from "../hooks/useSpotify";
+import useSongInfo from '../hooks/useSongInfo';
+
 
 function Player() {
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  // const [volume, setVolume] = useState(50);
+
+  const songInfo = useSongInfo();
+
   return (
-    <div>Player</div>
+    <div>
+      {/* Left */}
+      <div>
+        <img className='hidden md:inline h-10 w-10' src={songInfo?.album.images?.[0]?.url} alt='' />
+      </div>
+    </div>
   )
 }
 
