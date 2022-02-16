7b83c999f049b4b5304c9a6d9d40ac0486cf1ec6
SGrossett <selene_816@hotmail.com>
2022-02-16 16:35:36 -0500

    Remove scroll bar from Center component.

--- pomify/components/Center.js
+++ pomify/components/Center.js
@@ -36,7 +36,7 @@ function Center() {
   console.log(playlist);
 
   return (
-    <div className='flex-grow h-screen overflow-y-scroll'>
+    <div className='flex-grow h-screen overflow-y-scroll scrollbar-hide'>
       <UserIcon />
    
       <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${colour} h-80 text-white p-8`}>
