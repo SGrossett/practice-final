106c426e491010296c639dac9fbd7c8dc8f6ad12
SGrossett <selene_816@hotmail.com>
2022-02-15 22:35:10 -0500

    Create a song component to count the number of songs in a playlist, display the track image and relavent track information.

---
+++ pomify/components/Song.js
@@ -0,0 +1,23 @@
+function Song({ order, track }) {
+  return (
+    <div className='grid grid-cols-2'>
+      <div className='flex items-center space-x-4'>
+        <p>{order + 1}</p>
+          <img 
+            className='h-10 w-10'
+            src={track.track.album.images[0].url}
+            alt='' 
+          />
+          <div>
+            <p>{track.track.name}</p>
+            <p>{track.track.artists[0].name}</p>
+          </div>
+
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Song;
