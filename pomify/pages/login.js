290a850a01cafbadaa2fe77390e6e3eaba881698
KwekuDar <kwekudar@outlook.com>
2022-02-24 01:30:14 -0500

    changed the login page

--- pomify/pages/login.js
+++ pomify/pages/login.js
@@ -3,7 +3,11 @@ import {getProviders, signIn } from 'next-auth/react';
 function Login( { providers }) {
   return (
     <div className='flex flex-col items-center bg-black min-h-screen w-full justify-center'>
-      <img className='w-52 mb-5' src='https://links.papareact.com/9xl' alt='' />
+      <div className='flex ml-8 items-center justify-center'>
+        <img className='w-52 mb-5' src='https://d29fhpw069ctt2.cloudfront.net/icon/image/85329/preview.svg' alt='' />
+        <h1 className='text-white text-9xl'> Pomify </h1>
+      </div>
+     
 
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
